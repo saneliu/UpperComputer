5d66da71ba62726d912d1e736ab42a90bd74e07d
BugAngel <1055056138@qq.com>
2016-08-17 00:15:17 +0800

    first_commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace TEST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(105, 20);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "刷新";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(360, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 77);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(360, 144);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(170, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(455, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "发送";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 62);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "打开";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 223);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//添加这个类就可以使用SerialPort方法 获取可用串口
+using System.IO.Ports;
+
+namespace TEST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //线程设置为其他线程可以调用该线程的资源
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //让下拉框只能选择已有选项，不能输入
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            //自动搜索并且添加可用串口
+            string[] PortName = SerialPort.GetPortNames();//读取
+            for(int i=0;i<PortName.Length;i++)//添加
+            {
+                comboBox1.Items.Add(PortName[i]);
+            }
+
+            try
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+            catch
+            {
+                MessageBox.Show("没有可用端口");
+            }
+
+            //串口接受消息响应设置
+            serialPort1.DataReceived += serialPort1_DataReceived;
+        }
+
+        //串口接收响应方法  相当于单片机中断函数
+        void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+           // throw new NotImplementedException();
+            string ReceiveData;
+            //读取数据
+            ReceiveData = serialPort1.ReadLine();
+            if(ReceiveData[0]==1)
+            {
+                int i=0x01;
+                i++;
+            }
+        }
+
+        //串口刷新操作
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //清空之前的选项
+            comboBox1.Items.Clear();
+            //让下拉框只能选择已有选项，不能输入
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            //自动搜索并且添加可用串口
+            string[] PortName = SerialPort.GetPortNames();//读取
+            for (int i = 0; i < PortName.Length; i++)//添加
+            {
+                comboBox1.Items.Add(PortName[i]);
+            }
+
+            try 
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+            catch
+            {
+                MessageBox.Show("没有可用端口");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //发送数据
+            byte[] data = new byte[1];
+            data[0] = 1;
+            try
+            {
+                //第零位开始发  发一个
+                serialPort1.Write(data,0,1);
+            }
+            catch
+            {
+                MessageBox.Show("发送数据失败");
+            }
+        }
+
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //打开串口
+            if(serialPort1.IsOpen == false)
+            {
+                try
+                {
+                    serialPort1.Open();
+                    button3.Text="关闭";
+                }
+                catch
+                {
+                    MessageBox.Show("打开失败");
+                }
+            }
+            //关闭串口
+            else
+            {
+                try
+                {
+                    serialPort1.Close();
+                    button3.Text = "打开";
+                }
+                catch
+                {
+                    MessageBox.Show("关闭失败");
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //设置端口名称
+            serialPort1.PortName = comboBox1.Text;
+        }
+    }
+}
